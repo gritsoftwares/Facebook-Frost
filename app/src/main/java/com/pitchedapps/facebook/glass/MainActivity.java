8caddfba6985f3cf4a77d726d54600cff84c412b
Allan W <me@allanwang.ca>
2016-05-16 17:36:45 -0400

    checkpoint

--- app/src/main/java/com/pitchedapps/facebook/glass/MainActivity.java
+++ app/src/main/java/com/pitchedapps/facebook/glass/MainActivity.java
@@ -1,31 +1,90 @@
 package com.pitchedapps.facebook.glass;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+import com.pitchedapps.facebook.glass.fragments.DemoFragment;
 import com.sromku.simple.fb.SimpleFacebook;
-import com.pitchedapps.facebook.glass.fragments.MainFragment;
+import com.pitchedapps.facebook.glass.exampleFragments.MainFragment;
 import com.pitchedapps.facebook.glass.utils.Utils;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
     protected static final String TAG = MainActivity.class.getName();
 
     private SimpleFacebook mSimpleFacebook;
 
+    public static int[] tab10() {
+        return new int[] {
+                R.string.demo_tab_1,
+                R.string.demo_tab_2,
+                R.string.demo_tab_3,
+                R.string.demo_tab_4,
+                R.string.demo_tab_5,
+                R.string.demo_tab_6,
+                R.string.demo_tab_7,
+                R.string.demo_tab_8,
+                R.string.demo_tab_9,
+                R.string.demo_tab_10
+        };
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         mSimpleFacebook = SimpleFacebook.getInstance(this);
 
         // test local language
         Utils.updateLanguage(getApplicationContext(), "en");
 //        Utils.printHashKey(getApplicationContext());
 
-        setContentView(R.layout.activity_main);
-        addFragment();
+//        addFragment();
+
+        setContentView(R.layout.activity_demo);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        toolbar.setTitle("test demo");
+        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ViewGroup tab = (ViewGroup) findViewById(R.id.tab);
+        if (tab != null) { //TODO see if necessary
+            tab.addView(LayoutInflater.from(this).inflate(R.layout.demo_basic, tab, false));
+        }
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+
+        FragmentPagerItems pages = new FragmentPagerItems(this);
+        for (int titleResId : tab10()) {
+            pages.add(FragmentPagerItem.of(getString(titleResId), DemoFragment.class));
+        }
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), pages);
+
+        //TODO fix nullables
+        if (viewPager != null) {
+            viewPager.setAdapter(adapter);
+        }
+        if (viewPagerTab != null) {
+            viewPagerTab.setViewPager(viewPager);
+        }
+
     }
 
     @Override
@@ -46,5 +105,4 @@ public class MainActivity extends FragmentActivity {
         fragmentTransaction.add(R.id.frame_layout, new MainFragment());
         fragmentTransaction.commit();
     }
-
 }
---
+++ app/src/main/java/com/pitchedapps/facebook/glass/fragments/DemoFragment.java
@@ -0,0 +1,37 @@
+package com.pitchedapps.facebook.glass.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.pitchedapps.facebook.glass.R;
+
+/**
+ * Created by 7681 on 2016-05-16.
+ */
+public class DemoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        int position = FragmentPagerItem.getPosition(getArguments());
+        if (position == 3) {
+            return inflater.inflate(R.layout.fragment_demo_2, container, false);
+        }
+        return inflater.inflate(R.layout.fragment_demo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        int position = FragmentPagerItem.getPosition(getArguments());
+        TextView title = (TextView) view.findViewById(R.id.item_title);
+        title.setText(String.valueOf(position));
+    }
+
+}
